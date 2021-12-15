a7599302e8399a9805a1efbf0d61c8f045a2aa6b
xWTF <e@x.wtf>
2021-12-15 20:12:04 +0800

    Handle malformed Message-Id properly

--- mailboxresource.py
+++ mailboxresource.py
@@ -62,7 +62,7 @@ class MailboxClient:
 
     def getEmailFolder(self, msg, data):
         if msg['Message-Id']:
-            foldername = re.sub('[^a-zA-Z0-9_\-\.()\s]+', '', msg['Message-Id'])
+            foldername = re.sub('[^a-zA-Z0-9_\-\.() ]+', '', msg['Message-Id'])
         else:
             foldername = hashlib.sha224(data).hexdigest()
 
